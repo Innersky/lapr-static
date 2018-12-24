a3c258258760231ccb434da7fb42487691ccb90d
Bill <darkbluemn@gmail.com>
2018-12-23 21:26:45 -0500

    init

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends React.Component<{}, {}> {
-  render(): React.ReactNode {
-    return (
-      <div>123</div>
-    );
-  }
-}
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Users = () => <h2>Users</h2>;
 
-export default App;
+const AppRouter = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about/">About</Link>
+          </li>
+          <li>
+            <Link to="/users/">Users</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Index} />
+      <Route path="/about/" component={About} />
+      <Route path="/users/" component={Users} />
+    </div>
+  </Router>
+);
+
+export default AppRouter;
