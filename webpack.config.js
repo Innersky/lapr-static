53a738adc149a57dee59706e7e2758008de42a2f
Bill <darkbluemn@gmail.com>
2018-12-23 19:20:28 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/test.ts",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "."),
+    hot: true,
+    disableHostCheck: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
